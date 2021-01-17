dab3866c56a715e8dca5f485e09d77688f23e75a
Andy Lee <andylee3005@gmail.com>
2021-01-17 16:53:14 -0500

    Added security to controller.

--- AndyFXQ/src/main/java/lee/andyfxq/controller/FXQController.java
+++ AndyFXQ/src/main/java/lee/andyfxq/controller/FXQController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,32 +31,38 @@ public class FXQController {
 	//functions for mongodb
 	
 	@GetMapping("/list")
+//	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<FXQuote> requestFXQuote() {
 		return fxqServiceMongo.getFXQuote();
 	}
 	
 	@GetMapping("/symbol/{symbol}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<FXQuote> requestFXQuote(@PathVariable String symbol) {
 		return fxqServiceMongo.getQuoteSymbol(symbol);
 	}
 	
 	@GetMapping("/symbol/{symbol}/tenor/{tenor}")
+	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<FXQuote> requestFXQuote(@PathVariable String symbol, @PathVariable String tenor) {
 		return fxqServiceMongo.getQuoteSymbolTenor(symbol, tenor);
 	}
 
 	//functions to create data
 	@GetMapping("/slist")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<FXQuote> requestFXQuoteSorted() {
 		return fxqService.getFXQuote();
 	}
 	
 	@GetMapping("/ssymbol/{symbol}")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<FXQuote> requestFXQuoteSorted(@PathVariable String symbol) {
 		return fxqService.getQuoteSymbolSorted(symbol);
 	}
 	
 	@GetMapping("/ssymbol/{symbol}/tenor/{tenor}")
+	@PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 	public List<FXQuote> requestFXQuoteSorted(@PathVariable String symbol, @PathVariable String tenor) {
 		return fxqService.getQuoteSymbolTenorSorted(symbol, tenor);
 	}
